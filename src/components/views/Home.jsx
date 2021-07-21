0cfc072cb819ab213899e5fa8ab47b6221d0d2e6
Ninon Maraval <maravalninon@gmail.com>
2021-07-21 23:13:06 +0200

    added links on home

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import imagehome from '../../images/imagehome.jpg';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <h1>Bienvenue sur Plants handbook</h1>
-      <img src={imagehome} alt="home-img" />
+      <img className="home-pic" src={imagehome} alt="home-img" />
+      <p>Le carnet d&apos;entretien de vos plantes. </p>
+      <div className="home-buttons">
+        <Link to="/list">
+          <input type="button" value="Consulter mon carnet" />
+        </Link>
+        <Link to="/add">
+          <input type="button" value="Ajouter une plante à mon carnet" />
+        </Link>
+      </div>
     </div>
   );
 }
